21871e58183c73cb89ce570a818433e546d105d9
cjanowicz_Surface <cjanowicz@gmail.com>
2015-10-29 19:04:23 -0400

    Created title scene and implemented health UI
    Temp 3d health UI is in the game now, and I started on a title scene for
    the game.

--- Assets/Scripts/PlayerHealthScript.cs
+++ Assets/Scripts/PlayerHealthScript.cs
@@ -10,12 +10,20 @@ public class PlayerHealthScript : MonoBehaviour {
     private AudioSource m_audioSource;
     public AudioClip m_hurtSound;
     private Animator m_animator;
+	public TextMesh m_healthText;
 
 	// Use this for initialization
 	void Start () {
         m_health = m_maxHealth;
         m_audioSource = GetComponent<AudioSource>();
         m_animator = GetComponent<Animator>();
+		if(m_healthText == null){
+			m_healthText = GameObject.Find("HealthText").GetComponent<TextMesh>();
+			m_healthText.text = m_health.ToString();
+		} else{
+			m_healthText.text = m_health.ToString();
+		}
+
     }
 
 	void OnCollisionEnter(Collision collision) {
@@ -39,5 +47,10 @@ public class PlayerHealthScript : MonoBehaviour {
         m_health--;
         m_audioSource.PlayOneShot(m_hurtSound);
         m_animator.SetTrigger("GotHurt");
+		m_healthText.text = m_health.ToString();
+
+		if(m_health == 0){
+			///Start Death Sequence
+		}
     }
 }
