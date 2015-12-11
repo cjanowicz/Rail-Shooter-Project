22bfd5fcada991d5882ce0af70e1c7db24581cf7
cjanowicz_Surface <cjanowicz@gmail.com>
2015-12-11 18:37:32 -0500

    Added test tools to this branch.
    Test Tools added to this branch.

--- Assets/Scripts/LerpMoveScript.cs
+++ Assets/Scripts/LerpMoveScript.cs
@@ -4,10 +4,6 @@ public class LerpMoveScript : MonoBehaviour {
     public Transform m_target;
     private float m_damper = 0.5f;
 
-    // Use this for initialization
-    private void Start() {
-    }
-
     // Update is called once per frame
     private void Update() {
         transform.position = Vector3.Lerp(transform.position, m_target.position, m_damper);
